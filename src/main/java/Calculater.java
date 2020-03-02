06a4271cce6b8670fed022c80f70c46ae8c7602d
User <User@172.19.203.201>
2020-03-02 10:17:18 +0200

    HW2

---
+++ src/main/java/Calculater.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Calculater {
+
+	public Calculater() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+}
